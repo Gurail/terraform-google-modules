ca658c3b43765466dcb632fee6cb6d54849edd8c
Jason <jason.sun.9406@gmail.com>
2025-06-18 20:07:35 -0700

    support latest changes

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -68,8 +68,12 @@ resource "google_container_cluster" "this" {
 resource "google_container_node_pool" "default_nodepool" {
   name       = "default-pool"
   location   = var.region
-  cluster    = google_container_cluster.primary.name
+  cluster    = google_container_cluster.this.name
   node_count = var.node_count
+  
+  queued_provisioning {
+    enabled = false
+  }
 
   node_config {
     preemptible  = var.preemptible
@@ -77,10 +81,20 @@ resource "google_container_node_pool" "default_nodepool" {
     disk_size_gb = var.disk_size_gb
     disk_type    = var.disk_type
 
+    spot                        = true
+    advanced_machine_features {
+      enable_nested_virtualization = false
+      threads_per_core             = 0
+    }
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     service_account = var.service_account
     oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
+        "https://www.googleapis.com/auth/devstorage.read_only",
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/monitoring",
+        "https://www.googleapis.com/auth/service.management.readonly",
+        "https://www.googleapis.com/auth/servicecontrol",
+        "https://www.googleapis.com/auth/trace.append"
     ]
 
     labels = var.node_labels
--- modules/gke/outputs.tf
+++ modules/gke/outputs.tf
@@ -1,58 +1,58 @@
 output "cluster_name" {
   description = "The name of the GKE cluster"
-  value       = google_container_cluster.primary.name
+  value       = google_container_cluster.this.name
 }
 
 output "cluster_location" {
   description = "The location of the GKE cluster"
-  value       = google_container_cluster.primary.location
+  value       = google_container_cluster.this.location
 }
 
 output "cluster_endpoint" {
   description = "The IP address of the cluster master"
-  value       = google_container_cluster.primary.endpoint
+  value       = google_container_cluster.this.endpoint
   sensitive   = true
 }
 
 output "cluster_ca_certificate" {
   description = "The cluster CA certificate"
-  value       = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+  value       = google_container_cluster.this.master_auth.0.cluster_ca_certificate
   sensitive   = true
 }
 
 output "cluster_id" {
   description = "The unique identifier of the cluster"
-  value       = google_container_cluster.primary.id
+  value       = google_container_cluster.this.id
 }
 
 output "node_pool_name" {
   description = "The name of the node pool"
-  value       = google_container_node_pool.primary_nodes.name
+  value       = google_container_node_pool.default_nodepool.name
 }
 
 output "node_pool_instance_group_urls" {
   description = "The instance group URLs of the node pool"
-  value       = google_container_node_pool.primary_nodes.instance_group_urls
+  value       = google_container_node_pool.default_nodepool.instance_group_urls
 }
 
 output "kubeconfig_raw" {
   description = "Raw kubeconfig for the GKE cluster"
   value = {
-    cluster_name           = google_container_cluster.primary.name
-    endpoint              = google_container_cluster.primary.endpoint
-    cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+    cluster_name           = google_container_cluster.this.name
+    endpoint              = google_container_cluster.this.endpoint
+    cluster_ca_certificate = google_container_cluster.this.master_auth.0.cluster_ca_certificate
   }
   sensitive = true
 }
 
 output "kubernetes_cluster_host" {
   description = "The Kubernetes cluster host"
-  value       = google_container_cluster.primary.endpoint
+  value       = google_container_cluster.this.endpoint
   sensitive   = true
 }
 
 output "kubernetes_cluster_ca_certificate" {
   description = "The Kubernetes cluster CA certificate"
-  value       = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+  value       = base64decode(google_container_cluster.this.master_auth.0.cluster_ca_certificate)
   sensitive   = true
 }
